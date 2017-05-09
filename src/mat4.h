46ff810355f753cf21fbc44da8a85b239a136b02
Vitaliy Stoliarov <huccah@gmail.com>
2017-05-09 21:37:11 +0300

    getPosition transposed

--- src/mat4.h
+++ src/mat4.h
@@ -4,5 +4,5 @@ struct mat4 {
   float a[16];
   float operator[](size_t i) const { return a[i]; };
   float &operator[](size_t i) { return a[i]; };
-  vec3 getPosition() { return vec3{a[3], a[7], a[11]}; }
+  vec3 getPosition() { return vec3{a[12], a[13], a[14]}; }
 };
